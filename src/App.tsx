a0c4cd97903f152871b5ed8604e1b71fffb4de19
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-09-27 00:06:31 +0600

    Wrote interface for Ingredient. Added css styles for Burger. Also saved ingredient images to assets. Added array of ingredients and made button array for adding the ingredients to burger.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import "./App.css";
+import meatImage from "./assets/meat.png";
+import baconImage from "./assets/bacon.png";
+import lettuceImage from "./assets/lettuce.png";
+import cheeseImage from "./assets/cheese.png";
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Ingredient {
+  name: string;
+  price: number;
+  image: string;
+}
+
+const App = () => {
+//   const [ingredients, setIngredients] = useState([
+//
+//     {name: 'Meat': count: 0},
+//
+//   {name: 'Cheese': count: 0},
+//
+// ...
+//
+// ]};
+//   обычный map или forEach подойдет для поиска чтобы увеличивать count
+
+
+
+
+
+
+const INGREDIENTS: Ingredient[{name: string, price: number, image: string}] = [
+
+    {name: "meat", price: 80, image: meatImage},
+    {name: "bacon", price: 60, image: baconImage},
+    {name: "lettuce", price: 10, image: lettuceImage},
+    {name: "cheese", price: 50, image: cheeseImage},
+  ];
+
+  const object = {
+    meat: 0,
+    lettuce: 0,
+    bacon: 0,
+    cheese: 0,
+    // если у меня массив сделать по подсчету какую-нибудь map или цикл для объектов, чтобы определить на что я именно нажала
+  };
+
+  const getIngredientName = (ingredientName: string) => {
+    console.log(ingredientName)
+    object[ingredientName] ++;
+    console.log(object);
+  };
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="Burger">
+          <div className="BreadTop">
+            <div className="Seeds1"></div>
+            <div className="Seeds2"></div>
+          </div>
+          {/*<div className="Salad"></div>*/}
+          {/*<div className="Cheese"></div>*/}
+          {/*<div className="Meat"></div>*/}
+          <div className="BreadBottom"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      {INGREDIENTS.map(ingredient => (
+        <button style={{background: "none", border: "none"}} onClick={() => getIngredientName(ingredient.name)} key={ingredient.name} type="button"><img width={50} src={ingredient.image} alt={ingredient.name}/>
+        </button>
+      ))}
+
+    </>
+  );
+};
+
+export default App;
